9f8adca53bbf24d72e7cc9246648b36af7104aa7
Karim Kirillov <prjajkin.infro@gmail.com>
2020-12-22 15:14:55 +0300

    base animation system now works

---
+++ HumanGL/HumanGL/src/Animator.cpp
@@ -0,0 +1,72 @@
+//
+// Created by Jeromy Black on 12/19/20.
+//
+
+#include "Animator.h"
+
+Animator::Animator()
+{
+	animations["run"].emplace_back();
+	animations["run"].emplace_back();
+	animations["run"].emplace_back();
+	animations["run"].emplace_back();
+	animations["run"].emplace_back();
+	animations["run"].emplace_back();
+	animations["run"].emplace_back();
+	animations["run"].emplace_back();
+	animations["run"].emplace_back();
+	animations["run"].emplace_back();
+//	animations[0].AddAnimationKey(new AnimationKey(20.0f, vec3(0,0,0) , vec3(0,0,0),vec3(0,0,0)));
+//	animations[0].AddAnimationKey(new AnimationKey(20.0f,vec3(0,0,0), vec3(90,0,0), vec3(0,0,0)));
+//	animations[0].AddAnimationKey(new AnimationKey(20.0f, vec3(0,0,0) , vec3(180,0,0),vec3(0,0,0)));
+//	animations[0].AddAnimationKey(new AnimationKey(20.0f, vec3(0,0,0) , vec3(90,0,0),vec3(0,0,0)));
+
+	animations["run"][0].AddAnimationKey(new AnimationKey(20.f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+	animations["run"][0].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 90, 0), vec3(1, 1, 1)));
+	animations["run"][0].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+	animations["run"][0].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+
+	animations["run"][1].AddAnimationKey(new AnimationKey(20.f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+	animations["run"][1].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 30), vec3(1, 1, 1)));
+	animations["run"][1].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+	animations["run"][1].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+
+	animations["run"][2].AddAnimationKey(new AnimationKey(20.f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+	animations["run"][2].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 90, 90), vec3(1, 1, 1)));
+	animations["run"][2].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+	animations["run"][2].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+
+	animations["run"][3].AddAnimationKey(new AnimationKey(20.f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+	animations["run"][3].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 90), vec3(1, 1, 1)));
+	animations["run"][3].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+	animations["run"][3].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+
+	animations["run"][4].AddAnimationKey(new AnimationKey(20.f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+	animations["run"][4].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, -90), vec3(1, 1, 1)));
+	animations["run"][4].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+	animations["run"][4].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+
+	animations["run"][5].AddAnimationKey(new AnimationKey(20.f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+	animations["run"][5].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 90), vec3(1, 1, 1)));
+	animations["run"][5].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+	animations["run"][5].AddAnimationKey(new AnimationKey(20.0f, vec3(0, 0, 0), vec3(0, 0, 0), vec3(1, 1, 1)));
+
+
+
+
+//	animations[0].AddAnimationKey(new AnimationKey(5.0f, vec3(0,0,0) , vec3(180,0,0),vec3(0,0,0)));
+//	animations[0].AddAnimationKey(new AnimationKey(5.0f, vec3(0,0,0) , vec3(180,0,0),vec3(0,0,0)));
+
+
+
+
+//	animations[0].keys.emplace_back(new AnimationKey(6.0f, vec3(0,0,0) , vec3(0,0,0),vec3(0,0,0)));
+//	animations[0].keys.emplace_back(new AnimationKey(9.0f, vec3(0,0,0) , vec3(-180,0,0),vec3(0,0,0)));
+
+
+
+
+
+//	AnimationKey l1(3.0f,mat4(1), rotation, mat4(1));
+//	animations.at(0).keys.push_back(l1);
+}
